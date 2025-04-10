eb362557a461ae06305d724fd2439842298534f2
Jackblow33 <jackblow33@hotmail.com>
2025-04-10 19:13:16 -0400

    Create test.sh

---
+++ hw-install/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Execute as root
+#wifiinstall   Broadcom - imac BCM4360 & +++
+
+# Source: https://wiki.debian.org/wl
+
+# Check if the script is running as root
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
+# Backup the original sources.list file
+cp /etc/apt/sources.list /etc/apt/$TIMESTAMP.sources.list
+
+# Add the non-free contrib repository to the sources.list file
+echo "deb http://deb.debian.org/debian/ trixie non-free contrib" >> /etc/apt/sources.list
+
+# Update the package lists
+apt-get update
+
+echo "The non-free contrib repository has been added to the sources.list file."
