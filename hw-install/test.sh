b21e59da1f0f693c998ecce0abf9becf1db9ebb9
Jackblow33 <jackblow33@hotmail.com>
2025-04-10 19:43:16 -0400

    Update test.sh

--- hw-install/test.sh
+++ hw-install/test.sh
@@ -14,8 +14,24 @@ fi
 # Backup the original sources.list file
 cp /etc/apt/sources.list /etc/apt/$TIMESTAMP.sources.list
 
+#NEW -edit sources.list
+#echo "contrib non-free" | sudo tee -a /etc/apt/sources.list
+# OR
+#sudo sh -c 'echo "[some repository]" >> /etc/apt/sources.list'
+
+#NEW -add sources.list into /etc/apt/sources.list.d
+touch /etc/apt/sources.list.d/sources.list
+echo "# Modernized from /etc/apt/sources.list"                    >> /etc/apt/sources.list.d/sources.list
+echo "Types: deb deb-src"                                         >> /etc/apt/sources.list.d/sources.list
+echo "URIs: http://deb.debian.org/debian/"                        >> /etc/apt/sources.list.d/sources.list
+echo "Suites: trixie"                                             >> /etc/apt/sources.list.d/sources.list
+echo "Components: contrib non-free"                               >> /etc/apt/sources.list.d/sources.list
+echo "Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg"  >> /etc/apt/sources.list.d/sources.list
+
+
+# OLD
 # Add the non-free contrib repository to the sources.list file
-echo "deb http://deb.debian.org/debian/ trixie non-free contrib" >> /etc/apt/sources.list
+#echo "deb http://deb.debian.org/debian/ trixie non-free contrib" >> /etc/apt/sources.list
 
 # Update the package lists
 apt-get update
