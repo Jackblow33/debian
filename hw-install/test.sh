e5ba76c6f1a5e571e00b60236a6f377dce834a71
Jackblow33 <jackblow33@hotmail.com>
2025-04-10 20:05:32 -0400

    Update test.sh

--- hw-install/test.sh
+++ hw-install/test.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-#Execute as root
 #wifiinstall   Broadcom - imac BCM4360 & +++
 
 # Source: https://wiki.debian.org/wl
 
-# Check if the script is running as root
-if [ "$EUID" -ne 0 ]; then
-    echo "This script must be run as root."
-    exit 1
-fi
+#VARIABLE
+#SUITES=trixie  #Choose between trixie or sid
+
 
 # Backup the original sources.list file
 cp /etc/apt/sources.list /etc/apt/$TIMESTAMP.sources.list
@@ -20,13 +17,17 @@ cp /etc/apt/sources.list /etc/apt/$TIMESTAMP.sources.list
 #sudo sh -c 'echo "[some repository]" >> /etc/apt/sources.list'
 
 #NEW -add sources.list into /etc/apt/sources.list.d
-touch /etc/apt/sources.list.d/sources.list
-echo "# Modernized from /etc/apt/sources.list"                    >> /etc/apt/sources.list.d/sources.list
-echo "Types: deb deb-src"                                         >> /etc/apt/sources.list.d/sources.list
-echo "URIs: http://deb.debian.org/debian/"                        >> /etc/apt/sources.list.d/sources.list
-echo "Suites: trixie"                                             >> /etc/apt/sources.list.d/sources.list
-echo "Components: contrib non-free"                               >> /etc/apt/sources.list.d/sources.list
-echo "Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg"  >> /etc/apt/sources.list.d/sources.list
+# Create the sources.list file in the /etc/apt/sources.list.d
+sudo touch /etc/apt/sources.list.d/sources.list
+
+# Add the following text to the file
+echo "# Modernized from /etc/apt/sources.list" | sudo tee /etc/apt/sources.list.d/sources.list
+echo "Types: deb deb-src" | sudo tee /etc/apt/sources.list.d/sources.list
+echo "URIs: http://deb.debian.org/debian/" | sudo tee /etc/apt/sources.list.d/sources.list
+echo "Suites: trixie" | sudo tee /etc/apt/sources.list.d/sources.list
+echo "Components: main non-free-firmware contrib non-free" | sudo tee /etc/apt/sources.list.d/sources.list
+echo "Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg" | sudo tee /etc/apt/sources.list.d/sources.list
+
 
 
 # Update the package lists
