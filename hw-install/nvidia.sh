b94b52923016257f8d1ed574e1c4a6db2e964961
Jackblow33 <jackblow33@hotmail.com>
2025-04-12 12:51:05 -0400

    Update nvidia.sh

--- hw-install/nvidia.sh
+++ hw-install/nvidia.sh
@@ -19,26 +19,26 @@
 
 #fonctions
 #timer_start()
-{
-BEGIN=$(date +%s)
-}
+#{
+#BEGIN=$(date +%s)
+#}
 
 #fonctions
 #timer_stop()
-{
-    NOW=$(date +%s)
-    let DIFF=$(($NOW - $BEGIN))
-    let MINS=$(($DIFF / 60))
-    let SECS=$(($DIFF % 60))
-    echo Time elapsed: $MINS:`printf %02d $SECS`
-}
+#{
+#    NOW=$(date +%s)
+#    let DIFF=$(($NOW - $BEGIN))
+#    let MINS=$(($DIFF / 60))
+#    let SECS=$(($DIFF % 60))
+#    echo Time elapsed: $MINS:`printf %02d $SECS`
+#}
 
 
 # Check if the script is running as root
-if [ "$EUID" -ne 0 ]; then
-    echo "This script must be run as root."
-    exit 1
-fi
+#if [ "$EUID" -ne 0 ]; then
+#    echo "This script must be run as root."
+#    exit 1
+#fi
 
 
 #NVIDIA Driver install for 6.11 kernel +
