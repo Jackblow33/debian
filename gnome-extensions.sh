06bc08bbab26d480b1549efbd2c522a03827cd5e
Jackblow33 <jackblow33@hotmail.com>
2025-04-12 09:20:10 -0400

    Update gnome-extensions.sh

--- gnome-extensions.sh
+++ gnome-extensions.sh
@@ -14,11 +14,12 @@ cp -r ~/dash-to-dock@micxgx.gmail.com ~/.local/share/gnome-shell/extensions/
 sudo sensors-detect --auto #need lm-sensors dep
 sudo apt install  gnome-shell-extension-freon
 
-gnome-extensions enable dash-to-dock@micxgx.gmail.com
-gnome-extensions enable freon@UshakovVasilii_Github.yahoo.com
-gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
-gsettings set org.gnome.shell favorite-apps "['firefox-esr.desktop', 'org.gnome.gedit.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop']"
-#sudo systemctl restart gnome-shell
+# Enable extensnsions ans customize
+#gnome-extensions enable dash-to-dock@micxgx.gmail.com
+#gnome-extensions enable freon@UshakovVasilii_Github.yahoo.com
+#gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+#gsettings set org.gnome.shell favorite-apps "['firefox-esr.desktop', 'org.gnome.gedit.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop']"
+
 
 
 
