1a96abe0ac0fc5a98aada37de96cd9944f3e45f1
Jackblow33 <jackblow33@hotmail.com>
2025-04-11 20:29:45 -0400

    Update gnome-extensions.sh

--- gnome-extensions.sh
+++ gnome-extensions.sh
@@ -9,16 +9,15 @@ mv dash-to-dock dash-to-dock@micxgx.gmail.com
 cd dash-to-dock@micxgx.gmail.com
 sudo make && sudo make install     #Run the following commands to build the extension
 cp -r ~/dash-to-dock@micxgx.gmail.com ~/.local/share/gnome-shell/extensions/
-#gnome-extensions enable dash-to-dock@micxgx.gmail.com
-
 
 #freon gnome-shell-extension-sensors
 sudo sensors-detect --auto #need lm-sensors dep
 sudo apt install  gnome-shell-extension-freon
-#gnome-extensions enable freon@UshakovVasilii_Github.yahoo.com
 
-
-#gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+gnome-extensions enable dash-to-dock@micxgx.gmail.com
+gnome-extensions enable freon@UshakovVasilii_Github.yahoo.com
+gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+sudo systemctl restart gnome-shell
 
 
 
