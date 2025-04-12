2d11cca25371d4d805f97a7a1ce51d1260120dee
Jackblow33 <jackblow33@hotmail.com>
2025-04-12 12:12:36 -0400

    Update gnome-extensions.sh

--- gnome-extensions.sh
+++ gnome-extensions.sh
@@ -3,25 +3,29 @@
 #Gnome extensions
 
 #dash-to-dock
-mkdir ~/.local/share/gnome-shell/extensions/
+mkdir /home/$USR/.local/share/gnome-shell/extensions/
 git clone https://github.com/micheleg/dash-to-dock.git
 mv dash-to-dock dash-to-dock@micxgx.gmail.com
 cd dash-to-dock@micxgx.gmail.com
 sudo make && sudo make install     #Run the following commands to build the extension
-cp -r ~/dash-to-dock@micxgx.gmail.com ~/.local/share/gnome-shell/extensions/
+cp -r /home/$USR/dash-to-dock@micxgx.gmail.com /home/$USR/.local/share/gnome-shell/extensions/
 
 #freon gnome-shell-extension-sensors
 sudo sensors-detect --auto #need lm-sensors dep
 sudo apt install  gnome-shell-extension-freon
 
 # Enable extensnsions and customize
-#gnome-extensions enable dash-to-dock@micxgx.gmail.com
-#gnome-extensions enable freon@UshakovVasilii_Github.yahoo.com
-#gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+gnome-extensions enable dash-to-dock@micxgx.gmail.com
+gnome-extensions enable freon@UshakovVasilii_Github.yahoo.com
+gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+gsettings set org.gnome.shell.extensions.dash-to-dock show-volume false
+gsettings set org.gnome.shell.extensions.dash-to-dock show-device false
+gsettings set org.gnome.shell.extensions.dash-to-dock intellihide false
 
-#This will return a list of app IDs that are currently pinned
-#gsettings get org.gnome.shell favorite-apps then:
-#gsettings set org.gnome.shell favorite-apps "['firefox-esr.desktop', 'org.gnome.gedit.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop']"
+#This will return a list of app IDs that are currently pinned and help you generate the next command line following that one
+#gsettings get org.gnome.shell favorite-apps
+gsettings set org.gnome.shell favorite-apps "['firefox-esr.desktop', 'org.gnome.gedit.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop']" ### ADD varible to choosed web browser
+
 
 
 
