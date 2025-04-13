89c58dc8a318d6a5aaeab95f211748218b975e8c
Jackblow33 <jackblow33@hotmail.com>
2025-04-13 16:21:00 -0400

    Update debian-bookworm-trixie-sid.sh

--- debian-bookworm-trixie-sid.sh
+++ debian-bookworm-trixie-sid.sh
@@ -9,6 +9,7 @@ if ! grep -q "contrib" /etc/apt/sources.list || ! grep -q "non-free" /etc/apt/so
 else
     echo "contrib and non-free repositories are already present in /etc/apt/sources.list."
 fi
+    read -p "Press Enter to continue ..."
 
 
 
