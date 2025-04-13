d5fe31ca1bdd0377842d5848c1df02bdb3e57df1
Jackblow33 <jackblow33@hotmail.com>
2025-04-13 12:25:50 -0400

    Update DEB-nvidia-install.sh

--- hw-install/DEB-nvidia-install.sh
+++ hw-install/DEB-nvidia-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Source https://wiki.debian.org/NvidiaGraphicsDrivers
+# Must have proper repos enabled
 
 sudo apt update
 sudo apt install nvidia-detect
@@ -8,4 +9,5 @@ nvidia-detect
 echo '' ; echo '' ; echo ''
 read -p "Press Enter to start installing Nvidia driver ............................>>>"
 sudo apt install nvidia-driver  #for sid add: firmware-misc-nonfree
+#sudo systemctl --user enable --now nvidia-x11-xserver-settings.service
 
