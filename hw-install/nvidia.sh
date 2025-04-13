8cb192b0c391f83746083fe788de51cec9e09bab
Jackblow33 <jackblow33@hotmail.com>
2025-04-12 20:13:37 -0400

    Update nvidia.sh

--- hw-install/nvidia.sh
+++ hw-install/nvidia.sh
@@ -27,7 +27,7 @@
     #apt autoremove $(dpkg -l nvidia-driver* |grep ii |awk '{print $2}')
     
                       #Working too apt install -y pkg-config libglvnd-dev dkms build-essential libegl-dev libegl1 libgl-dev libgl1 libgles-dev libgles1 libglvnd-core-dev libglx-dev libopengl-dev gcc make pkg-config linux-headers-$(uname -r)
-                      sudo apt install linux-headers-$(uname -r) build-essential libglvnd-dev pkg-config
+                      sudo apt install -y linux-headers-$(uname -r) build-essential libglvnd-dev pkg-config
     #WORKING apt install -y linux-headers-$(uname -r) gcc make acpid dkms libglvnd-core-dev libglvnd0 libglvnd-dev libc-dev pkg-config
     wget https://us.download.nvidia.com/XFree86/Linux-x86_64/570.133.07/NVIDIA-Linux-x86_64-"$NV_VER".run
     sudo chmod +x NVIDIA-Linux-x86_64-"$NV_VER".run
@@ -38,7 +38,7 @@
     sudo systemctl stop gdm
     sudo systemctl stop gdm3 || sudo systemctl stop lightdm
 
-    read -p "Press Enter to start installing Nvidia driver ............................>>>"
+    #read -p "Press Enter to start installing Nvidia driver ............................>>>"
 
     
 ./NVIDIA-Linux-x86_64-"$NV_VER".run
