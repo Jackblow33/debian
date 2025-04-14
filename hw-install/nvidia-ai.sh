7c4349bb6691cd1db5859bfcc94b4e3bfb4a1c8b
Jackblow33 <jackblow33@hotmail.com>
2025-04-13 22:10:46 -0400

    Update nvidia-ai.sh

--- hw-install/nvidia-ai.sh
+++ hw-install/nvidia-ai.sh
@@ -25,14 +25,14 @@ wget "https://us.download.nvidia.com/XFree86/Linux-x86_64/${NV_VER}/NVIDIA-Linux
 chmod +x "NVIDIA-Linux-x86_64-${NV_VER}.run"
 
 # Stop display manager services
-systemctl stop gdm gdm3 lightdm || handle_error
+systemctl stop gdm gdm3 lightdm
 
 read -p "Press Enter to start installing NVIDIA driver ............................>>>"
 # Install NVIDIA driver
 ./NVIDIA-Linux-x86_64-"$NV_VER".run || handle_error
 
 # Backup and update GRUB configuration
-cp /etc/default/grub.d/nvidia-modeset.cfg /etc/default/grub.d/nvidia-modeset.cfg.$(date +%Y%m%d%H%M%S) || handle_error
+cp /etc/default/grub.d/nvidia-modeset.cfg /etc/default/grub.d/nvidia-modeset.cfg.$(date +%Y%m%d%H%M%S)
 echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX nvidia-drm.modeset=1"' > /etc/default/grub.d/nvidia-modeset.cfg
 update-grub || handle_error
 
