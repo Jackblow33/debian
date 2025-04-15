6bbb012ca717c27070bbcacd91138e5747f6f8ca
Jackblow33 <jackblow33@hotmail.com>
2025-04-14 21:40:38 -0400

    Add files via upload

--- hw-install/nvidia.sh
+++ hw-install/nvidia.sh
@@ -1,196 +1,75 @@
 #!/bin/bash
-#nvidia.sh
 
-# Execute as root 
-## linux kernel 6.11 and beyond required
-# Switch to the terminal view of your system by pressing Ctrl + Alt + F3 and then launch the script ./nvidia1.sh
-# Install Nvidia driver
+#2025-04-14
 
+# nvidia.sh - Script to install NVIDIA drivers on Debian 12 - Trixie & Sid. Untested on Stable but might work.
 
+# Execute as root
+# Linux kernel 6.11 and beyond required
+# Switch to the terminal view of your system by pressing Ctrl + Alt + F3.
+# chmod +x nvidia.sh
+# Then launch the script:   ./nvidia.sh
+
+# Function to handle errors
+handle_error() {
+    echo "Error occurred in the script. Exiting."
+    exit 1
+}
+
+# Start timer
 timer_start
-    #apt update           # && apt upgrade -y
-    #apt-get remove --purge '^nvidia-.*'
-    #apt purge libnvidia-*
-    #apt autoremove
-    #apt autoremove $(dpkg -l nvidia-driver* |grep ii |awk '{print $2}')
-    sleep 2
-    #apt install pkg-config libglvnd-dev dkms build-essential libegl-dev libegl1 libgl-dev libgl1 libgles-dev libgles1 libglvnd-core-dev libglx-dev libopengl-dev gcc make pkg-config linux-headers-$(uname -r)
-    apt install linux-headers-$(uname -r) build-essential libglvnd-dev pkg-config
-    sleep 2                #WORKING apt install -y linux-headers-$(uname -r) gcc make acpid dkms libglvnd-core-dev libglvnd0 libglvnd-dev libc-dev pkg-config #dracut
-    wget https://us.download.nvidia.com/XFree86/Linux-x86_64/570.133.07/NVIDIA-Linux-x86_64-"$NV_VER".run
-    sleep 1
-    sudo chmod +x NVIDIA-Linux-x86_64-"$NV_VER".run
-                      
-    
 
-# Stop the GDM service - Just in case on
-    sudo systemctl stop gdm
-    sudo systemctl stop gdm3 || sudo systemctl stop lightdm
+# Update package list and install necessary packages
+# apt update && apt install -y linux-headers-$(uname -r) build-essential dkms libglvnd-dev pkg-config || handle_error
+apt update && apt install -y linux-headers-$(uname -r) gcc make acpid dkms libglvnd-core-dev libglvnd0 libglvnd-dev libc-dev pkg-config || handle_error
 
-    read -p "Press Enter to start installing Nvidia driver ............................>>>"
+# Download NVIDIA driver
+# NV_VER="570.133.07"  # Set your NVIDIA version here if you are not using install.sh to reach this script.
+wget "https://us.download.nvidia.com/XFree86/Linux-x86_64/${NV_VER}/NVIDIA-Linux-x86_64-${NV_VER}.run" || handle_error
+chmod +x "NVIDIA-Linux-x86_64-${NV_VER}.run"
 
-    
-./NVIDIA-Linux-x86_64-"$NV_VER".run
+# Stop display manager services
+systemctl stop gdm gdm3 lightdm  # error expected
 
-    
-    cp /etc/default/grub.d/nvidia-modeset.cfg /etc/default/grub.d/nvidia-modeset.cfg.$TIMESTAMP
-    rm -f /etc/default/grub.d/nvidia-modeset.cfg
-    echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX nvidia-drm.modeset=1"' >> /etc/default/grub.d/nvidia-modeset.cfg
-    nano /etc/default/grub.d/nvidia-modeset.cfg
-    update-grub
+#read -p "Press Enter to start installing NVIDIA driver ............................>>>"
+# Install NVIDIA driver
+./NVIDIA-Linux-x86_64-"$NV_VER".run || handle_error
 
-# FIX Gnome - source https://wiki.archlinux.org/title/GDM#Wayland_and_the_proprietary_NVIDIA_driver    
-    ln -s /dev/null /etc/udev/rules.d/61-gdm.rules   #sudo ???
+# Backup and update GRUB configuration
+cp /etc/default/grub.d/nvidia-modeset.cfg /etc/default/grub.d/nvidia-modeset.cfg.$(date +%Y%m%d%H%M%S)  # error expected
+echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX nvidia-drm.modeset=1"' > /etc/default/grub.d/nvidia-modeset.cfg
+update-grub || handle_error
 
+# Fix Gnome for NVIDIA
+ln -sf /dev/null /etc/udev/rules.d/61-gdm.rules || handle_error
 
-# FIX NVIDIA - Graphical glitches and unresponsive after waking from sleep
-# Source  https://wiki.archlinux.org/title/NVIDIA/Tips_and_tricks#Preserve_video_memory_after_suspend
-  echo 'options nvidia NVreg_PreserveVideoMemoryAllocations=1' >>  /etc/modprobe.d/nvidia-power-management.conf
-  echo '#NVreg_TemporaryFilePath=/var/tmp' >>  /etc/modprobe.d/nvidia-power-management.conf
-  nano /etc/modprobe.d/nvidia-power-management.conf
+# Fix NVIDIA graphical glitches after waking from sleep
+echo 'options nvidia NVreg_PreserveVideoMemoryAllocations=1' >> /etc/modprobe.d/nvidia-power-management.conf
+echo '#NVreg_TemporaryFilePath=/var/tmp' >> /etc/modprobe.d/nvidia-power-management.conf
 
-# Making sure next 3 services are enable  --options enable(default), disable & status
-#VARIABLE
-OPTION=enable    #disable, status
-  systemctl $OPTION nvidia-suspend.service
-  systemctl $OPTION nvidia-hibernate.service
-  systemctl $OPTION nvidia-resume.service 
-  
-  sudo update-initramfs -u
+# Enable necessary services
+for service in nvidia-suspend.service nvidia-hibernate.service nvidia-resume.service; do
+    systemctl enable "$service" || handle_error
+done
+
+# Update initramfs
+update-initramfs -u || handle_error
+
+# Stop timer
 timer_stop
-  clear
-  echo ''
-  echo ''
-  echo ''
-  read -p "$(echo -e $GREEN"Installation completed! Press Enter ...........................>>> "$NC)"
- #sudo shutdown -r now    #reboot
 
-    # Checks
-    #sudo cat /sys/module/nvidia_drm/parameters/modeset   #Y is expected nvidia-drm modeset is enable (modeset=1)
-    #sudo cat /sys/module/nvidia_drm/parameters/fbdev     #Nothing expected there. fbdev driver is generally considered deprecated in favor of DRM (modeset=1)
-    #sudo cat /proc/driver/nvidia/params | grep "PreserveVideoMemoryAllocations" #1 is expected if PreserveVideoMemoryAllocations is enable
-    #glxinfo | egrep "OpenGL vendor|OpenGL renderer*"    # Check if using Mesa driver instead of the NVIDIA one on Wayland sessions - NVIDIA expected
-    #glxinfo | grep NVIDIA
-    #lsmod | grep nouveau
-    #lspci -nn | egrep -i "3d|display|vga" #gpu identification
-    #nvidia-smi
+# Clear the screen and notify the user
+clear
+echo -e "\n\n\nInstallation completed! Press Enter to continue..."
+read
 
-#---------------------------------------------------------------------------------------------------------------------
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    #If you plan to use suspend/hibernate functionality under KDE-Gnome desktop environment, you may want to add another option to avoid graphics "glitches" after wakeup/restore:
-### Warning: skip this step if you have Optimus hybrid graphics
-#sudo echo "options nvidia NVreg_PreserveVideoMemoryAllocations=1" >> /etc/modprobe.d/nvidia-options.conf
+# Uncomment to reboot automaticaly at the end of the installation
+# shutdown -r now
 
-
-
-
-
-
-#Blacklist Nouveau driver ---DISABLE--- Nvidia installer seems to do it properly
-    #cp /etc/modprobe.d/blacklist-nvidia-nouveau.conf /etc/modprobe.d/blacklist-nvidia-nouveau_$TIMESTAMP.conf
-    #bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-    #bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-    #cat <<EOF | sudo tee /etc/modprobe.d/blacklist-nvidia-nouveau.conf 
-    #> blacklist nouveau
-    #> options nouveau modeset =0
-    #>EOF
-    #or /etc/modprobe.d/blacklist-nouveau.conf
-    #update-initramfs -u
-#reboot
-
-
-
-
-    
-    
-    #UBUNTU references
-    #echo "Once the installer has completed installing the driver, run sudo update-initramfs -u to update the initramfs."
-    ###echo "Edit /etc/default/grub using sudo nano /etc/default/grub"
-    ###echo 'Add nvidia-drm.modeset=1 and nvidia-drm.fbdev=1 inside your GRUB_CMDLINE_LINUX (i.e. GRUB_CMDLINE_LINUX="nvidia-drm.modeset=1 nvidia-drm.fbdev=1")'
-    #echo 'Need to add the nvidia-drm.modeset=1 to the grub config for your kernel. Create the file /etc/default/grub.d/nvidia-modeset.cfg'
-    #echo 'Add the line:'
-    #echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX nvidia-drm.modeset=1"'
-    #echo "Run sudo update-grub"
-    #echo "Reboot the system"
-    #echo "Your newly installed driver should be up and running once the system boots up (you may run nvidia-smi to confirm so)."
-    #read -p "Press    -Enter-   ............................>>>"
-#Source https://github.com/oddmario/NVIDIA-Ubuntu-Driver-Guide/tree/main?tab=readme-ov-file#installing-through-the-official-nvidia-installer-from-the-nvidiacom-website
-
-
-
-
-
-
-
-
-#For Debian Trixie           OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD
-#NV Version 535.216.03
-
-#Source https://wiki.debian.org/NvidiaGraphicsDrivers
-
-#GPU Identification
-#lspci -nn | egrep -i "3d|display|vga"
-
-#Check for driver version before installing
-# If driver: nouveau. Then you have open sources driver. Update might be a good idea...
-#lspci -v
-
-#Add contrib non-free & non-free-firmware components to /etc/apt/sources.list
-
-#detect gpu and suggest drivers
-#sudo apt install nvidia-detect
-#nvidia-detect
-
-#If your system uses dracut  - NOT in my case
-#Make a dracut configuration file /etc/dracut.conf.d/10-nvidia.conf with this
-#install_items+=" /etc/modprobe.d/nvidia-blacklists-nouveau.conf /etc/modprobe.d/nvidia.conf /etc/modprobe.d/nvidia-options.conf "
-
-#sudo apt update
-#To install "proprietary" flavor, packages 
-#sudo apt install nvidia-kernel-dkms nvidia-driver firmware-misc-nonfree
-#To install "open" flavor,
-#sudo apt install nvidia-open-kernel-dkms nvidia-driver firmware-misc-nonfree
-#DKMS will build the nvidia module for your system
-
-#Restart your system to load the new driver
-
-#wayland desktop
-#sudo cat /sys/module/nvidia_drm/parameters/modeset
-#If this command returns N you need to set nvidia-drm modeset by adding options nvidia-drm modeset=1 to /etc/modprobe.d/nvidia-options.conf. For example
-#sudo echo "options nvidia-drm modeset=1" >> /etc/modprobe.d/nvidia-options.conf
-
-
-#If you plan to use suspend/hibernate functionality under KDE desktop environment, you may want to add another option to avoid graphics "glitches" after wakeup/restore:
-### Warning: skip this step if you have Optimus hybrid graphics
-#sudo echo "options nvidia NVreg_PreserveVideoMemoryAllocations=1" >> /etc/modprobe.d/nvidia-options.conf
-
-#Then reboot your system (check again if modeset is properly set, as described above) and Nvidia card should be properly recognized and used under wayland desktop.
+# Optional checks
+# sudo cat /sys/module/nvidia_drm/parameters/modeset
+# sudo cat /proc/driver/nvidia/params | grep "PreserveVideoMemoryAllocations"
+# glxinfo | egrep "OpenGL vendor|OpenGL renderer*"
+# lsmod | grep nouveau
+# lspci -nn | egrep -i "3d|display|vga"
+# nvidia-smi
