73a45862391e6cab117e8c6f525aab7e89a63f52
Jackblow33 <jackblow33@hotmail.com>
2025-04-21 17:25:41 -0400

    Update qemu-kvm-0.4.sh

--- qemu-kvm-0.4.sh
+++ qemu-kvm-0.4.sh
@@ -5,22 +5,74 @@
 ######################################
 #Mostly based on: https://github.com/daveprowse/virtualization/blob/main/kvm/kvm-install-debian-12/kvm-install-debian-12.md
 
-#echo '##Cpu core count##'
-#egrep -c '(vmx|svm)' /proc/cpuinfo
-#echo 'Check if virtualisation is enable in bios  #Intel Cpu should read: Virtualization: VT-x'
-#lscpu | grep Virtualization
-#                                    add check for Intel Amd virtualisation
-#read -p "Press enter to start"
+# Copy before editing /etc/default/grub
+GRUB_FILE="/etc/default/grub"
+cp $GRUB_FILE $GRUB_FILE.$TIMESTAMP || handle_error
+
+# Search for the line: GRUB_CMDLINE_LINUX_DEFAULT="quiet*
+GRUB_LINE=$(grep -E '^GRUB_CMDLINE_LINUX_DEFAULT="quiet' "$GRUB_FILE")
+
+# Check if the line was found
+if [ -z "$GRUB_LINE" ]; then
+  echo "Error: Could not find the GRUB_CMDLINE_LINUX_DEFAULT line in $GRUB_FILE."
+  exit 1
+fi
+
+# Check if the intel_iommu=on argument is already present                 # ADD AMD ######################################
+if grep -q 'intel_iommu=on' <<< "$GRUB_LINE"; then
+  echo "The intel_iommu=on argument is already present in the GRUB_CMDLINE_LINUX_DEFAULT line."
+  exit 0
+fi
+
+# Append the intel_iommu=on argument to the line
+NEW_GRUB_LINE="${GRUB_LINE%\"} intel_iommu=on\""
+echo "Updating the GRUB_CMDLINE_LINUX_DEFAULT line in $GRUB_FILE:"
+echo "  Old line: $GRUB_LINE"
+echo "  New line: $NEW_GRUB_LINE"
+
+# Update the GRUB_CMDLINE_LINUX_DEFAULT line in the file
+sed -i "s|$GRUB_LINE|$NEW_GRUB_LINE|" "$GRUB_FILE"
+echo "Successfully updated the GRUB_CMDLINE_LINUX_DEFAULT line in $GRUB_FILE."
+
+
+# Update the GRUB configuration
+update-grub || handle_error
+
 apt update
-apt install qemu-kvm qemu-utils libvirt-daemon-system libvirt-clients virtinst virt-manager
+apt install -y qemu-kvm qemu-utils libvirt-daemon-system libvirt-clients virtinst virt-manager || error_handler
 
 #Enable libvirtd
-systemctl --now enable libvirtd
-#read -p "Press enter to start"
+systemctl --now enable libvirtd || error_handler
 
-##Check the service status:
-#systemctl status libvirtd
-# Ctrl+c to exit
+# Check the service status:
+systemctl status libvirtd || error_handler
+
+# Add a user to the libvirt group so that it can create and modify virtual machines
+usermod -aG libvirt $USR
+usermod -aG libvirt-qemu $USR
+usermod -aG kvm $USR
+usermod -aG input $USR
+usermod -aG disk $USR
+
+
+# Set Virsh to autostart whenever the system is rebooted
+sudo virsh net-autostart default
+
+# Restarting the libvirtd service
+systemctl restart libvirtd
+
+
+# Extra checks  ####################################################
+# Let's view our virsh network:
+# virsh net-list
+
+# #Run the following command to view the various components that should run in KVM:
+# sudo virt-host-validate
+# It is normal to have freezer FAIL and secure guest support WARN. Qemu related lines have to be green
+
+# Check the service status:
+# systemctl status libvirtd
+
 #Check QEMU and Virsh versions:
 #kvm --version
 ##Check that the KVM modules are loaded correctly.
@@ -35,31 +87,14 @@ systemctl --now enable libvirtd
 #kvm                  1142784  1 kvm_intel
 #read -p "Press enter to start"
 
-##Run the following command to view the various components that should run in KVM:
-#sudo virt-host-validate
-#It is normal to have freezer FAIL and secure guest support WARN. Qemu related lines have to be green.
-#read -p "Press enter to start"
+#echo '##Cpu core count##'
+#egrep -c '(vmx|svm)' /proc/cpuinfo
+#echo 'Check if virtualisation is enable in bios  #Intel Cpu should read: Virtualization: VT-x'
+#lscpu | grep Virtualization
+#                                    add check for Intel Amd virtualisation
 
-# Add a user to the libvirt group so that it can create and modify virtual machines.
-sudo usermod -aG libvirt $USR
-#sudo usermod -aG libvirt-qemu $USR
-#sudo usermod -aG kvm $USR
-#sudo usermod -aG input $USR
-#sudo usermod -aG disk $USR
-read -p "Press enter to start"
 
-##And, set it to autostart whenever the system is rebooted.
-sudo virsh net-autostart default
-#read -p "Press enter to start"
 
-#Now apply the changes. These can be applied by restarting the libvirtd service or by restarting the computer.
-systemctl restart libvirtd
-#read -p "Press enter to start"
-
-## Let's view our virsh network:
-#virsh net-list
-read -p "Press enter"
-#reboot
 
 
 
