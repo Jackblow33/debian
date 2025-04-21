d5343c5ca27904b8739d55fa92ffd73be4c0cea5
Jackblow33 <jackblow33@hotmail.com>
2025-04-21 16:35:26 -0400

    Update qemu-kvm-0.4.sh

--- qemu-kvm-0.4.sh
+++ qemu-kvm-0.4.sh
@@ -5,27 +5,27 @@
 ######################################
 #Mostly based on: https://github.com/daveprowse/virtualization/blob/main/kvm/kvm-install-debian-12/kvm-install-debian-12.md
 
-echo '##Cpu core count##'
-egrep -c '(vmx|svm)' /proc/cpuinfo
-echo 'Check if virtualisation is enable in bios  #Intel Cpu should read: Virtualization: VT-x'
-lscpu | grep Virtualization
+#echo '##Cpu core count##'
+#egrep -c '(vmx|svm)' /proc/cpuinfo
+#echo 'Check if virtualisation is enable in bios  #Intel Cpu should read: Virtualization: VT-x'
+#lscpu | grep Virtualization
 #                                    add check for Intel Amd virtualisation
-read -p "Press enter to start"
+#read -p "Press enter to start"
 apt update
 apt install qemu-kvm qemu-utils libvirt-daemon-system libvirt-clients virtinst virt-manager
 
 #Enable libvirtd
 systemctl --now enable libvirtd
-read -p "Press enter to start"
+#read -p "Press enter to start"
 
 ##Check the service status:
-systemctl status libvirtd
+#systemctl status libvirtd
 # Ctrl+c to exit
 #Check QEMU and Virsh versions:
-kvm --version
+#kvm --version
 ##Check that the KVM modules are loaded correctly.
-lsmod | grep kvm
-read -p "Press enter, libvirtd should be enabled now"
+#lsmod | grep kvm
+#read -p "Press enter, libvirtd should be enabled now"
 
 
 #lsmod | grep kvm should return:
@@ -33,12 +33,12 @@ read -p "Press enter, libvirtd should be enabled now"
 #dpro@smauggy:~$ lsmod | grep kvm
 #kvm_intel             380928  0
 #kvm                  1142784  1 kvm_intel
-read -p "Press enter to start"
+#read -p "Press enter to start"
 
 ##Run the following command to view the various components that should run in KVM:
-sudo virt-host-validate
+#sudo virt-host-validate
 #It is normal to have freezer FAIL and secure guest support WARN. Qemu related lines have to be green.
-read -p "Press enter to start"
+#read -p "Press enter to start"
 
 # Add a user to the libvirt group so that it can create and modify virtual machines.
 sudo usermod -aG libvirt $USR
@@ -50,14 +50,14 @@ read -p "Press enter to start"
 
 ##And, set it to autostart whenever the system is rebooted.
 sudo virsh net-autostart default
-read -p "Press enter to start"
+#read -p "Press enter to start"
 
 #Now apply the changes. These can be applied by restarting the libvirtd service or by restarting the computer.
 systemctl restart libvirtd
-read -p "Press enter to start"
+#read -p "Press enter to start"
 
 ## Let's view our virsh network:
-virsh net-list
+#virsh net-list
 read -p "Press enter"
 #reboot
 
