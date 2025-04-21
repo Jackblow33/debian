5c9e56d0c8c554128df96bacddd971d2c13cf389
Jackblow33 <jackblow33@hotmail.com>
2025-04-20 22:44:06 -0400

    Update firefox.sh

--- firefox.sh
+++ firefox.sh
@@ -3,6 +3,18 @@
 # Update the package index
 #sudo apt update
 
+# Add Mozilla APT repository to sources list
+echo 'deb http://download.mozilla.org/ apt stable' | sudo tee /etc/apt/sources.list.d/mozilla.list
+# Import the Mozilla repository key
+
+# Update your package list and install Firefox
+apt update && apt install firefox
+
+
+
+
+
+
 # Install the required dependencies
 sudo apt install -y software-properties-common wget || handle_error
 echo -e "\n\n\nPress Enter..."
