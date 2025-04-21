dd8d4df38ce9d1c65e9b41fb6c7dda8a7939613b
Jackblow33 <jackblow33@hotmail.com>
2025-04-21 00:30:08 -0400

    Create kvm-qemu-0.1.sh

---
+++ kvm-qemu-0.1.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+######################################
+# KVM - Qemu without gpu passtrough ##
+######################################
+#Mostly based on: https://github.com/daveprowse/virtualization/blob/main/kvm/kvm-install-debian-12/kvm-install-debian-12.md
+
+USR=$(logname) 
+
+echo '##Cpu core count##'
+egrep -c '(vmx|svm)' /proc/cpuinfo
+echo 'Check if virtualisation is enable in bios  #Intel Cpu should read: Virtualization: VT-x'
+lscpu | grep Virtualization
+#                                    add check for Intel Amd virtualisation
+read -p "Press enter to start"
+sudo apt update
+sudo apt install qemu-kvm qemu-utils libvirt-daemon-system libvirt-clients virtinst virt-manager
+
+
+# Append iommu entrie in the grub file
+# Backup the original /etc/default/grub file
+cp /etc/default/grub /etc/default/grub.$TIMESTAMP
+
+# Check if 'intel_iommu=on' is already present in GRUB_CMDLINE_LINUX_DEFAULT
+if ! grep -q 'intel_iommu=on' /etc/default/grub; then
+    # Edit the /etc/default/grub file
+    echo 'GRUB_CMDLINE_LINUX_DEFAULT "intel_iommu=on"' >> /etc/default/grub
+    nano /etc/default/grub
+    
+    # Update the GRUB configuration
+    update-grub
+
+    update-initramfs -u || handle_error
+
+    #After rebooting, you can verify that the Intel IOMMU is enabled by checking the kernel parameters
+    cat /proc/cmdline
+    #check the dmesg output for IOMMU-related messages
+    dmesg | grep -i iommu    # = DMAR: IOMMU enabled
+
+    read -p "Press enter to continue"
+    #reboot
+    
+#    echo "The /etc/default/grub file has been updated with 'intel_iommu=on' added to the GRUB_CMDLINE_LINUX_DEFAULT variable."
+#    echo "The GRUB configuration has been updated. Please reboot your system for the changes to take effect."
+
+#Enable libvirtd
+systemctl --now enable libvirtd
+read -p "Press enter to start"
+
+##Check the service status:
+systemctl status libvirtd
+# Ctrl+c to exit
+#Check QEMU and Virsh versions:
+kvm --version
+##Check that the KVM modules are loaded correctly.
+lsmod | grep kvm
+read -p "Press enter, libvirtd should be enabled now"
+
+
+#lsmod | grep kvm should return:
+#Sample results:
+#dpro@smauggy:~$ lsmod | grep kvm
+#kvm_intel             380928  0
+#kvm                  1142784  1 kvm_intel
+read -p "Press enter to start"
+
+##Run the following command to view the various components that should run in KVM:
+sudo virt-host-validate
+#It is normal to have freezer FAIL and secure guest support WARN. Qemu related lines have to be green.
+read -p "Press enter to start"
+
+# Add a user to the libvirt group so that it can create and modify virtual machines.
+sudo usermod -aG libvirt $USR
+#sudo usermod -aG libvirt-qemu $USR
+#sudo usermod -aG kvm $USR
+#sudo usermod -aG input $USR
+#sudo usermod -aG disk $USR
+read -p "Press enter to start"
+
+##And, set it to autostart whenever the system is rebooted.
+sudo virsh net-autostart default
+read -p "Press enter to start"
+
+#Type the following command:
+#sudo virsh net-list
+
+#Now apply the changes. These can be applied by restarting the libvirtd service or by restarting the computer.
+systemctl restart libvirtd
+read -p "Press enter to start"
+
+##Now, let's view our KVM networks again:
+#sudo virsh net-list
+#After rebooting, you can verify that the Intel IOMMU is enabled by checking the kernel parameters
+#cat /proc/cmdline
+#check the dmesg output for IOMMU-related messages
+#dmesg | grep -i iommu    # = DMAR: IOMMU enabled
+echo "The GRUB configuration has been updated. Please reboot your system for the changes to take effect."
+read -p "Press enter to reboot"
+reboot
