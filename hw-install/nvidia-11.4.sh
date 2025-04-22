50418de79e51c609a5ba2706216774bd28021366
Jackblow33 <jackblow33@hotmail.com>
2025-04-22 18:22:26 -0400

    Update nvidia-11.4.sh

--- hw-install/nvidia-11.4.sh
+++ hw-install/nvidia-11.4.sh
@@ -49,7 +49,7 @@ timer_stop() {
 
 # Update package list and install necessary packages
 install_dependencies() {
-    apt update && apt install -y linux-headers-$(uname -r) gcc make acpid dkms libvulkan1 libglvnd-core-dev pkg-config || handle_error  #libglvnd0 libglvnd-dev libc-dev
+    apt update && apt install -y linux-headers-$(uname -r) gcc make acpid dkms libvulkan1 libglvnd-core-dev pkg-config xorg-dev || handle_error  #libglvnd0 libglvnd-dev libc-dev
 }
 
 # Download NVIDIA driver
