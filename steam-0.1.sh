834bbda04b2a313cd48a6d3a2c5e424dcc2a9bac
Jackblow33 <jackblow33@hotmail.com>
2025-04-21 21:46:44 -0400

    Update steam-0.1.sh

--- steam-0.1.sh
+++ steam-0.1.sh
@@ -3,6 +3,10 @@
 # Debian Steam install
 
 
-curl -s http://repo.steampowered.com/steam/archive/stable/steam.gpg | sudo tee /usr/share/keyrings/steam.gpg > /dev/null || error_handler
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/steam.gpg] http://repo.steampowered.com/steam/ stable steam' | sudo tee /etc/apt/sources.list.d/steam.list || error_handler
-apt update && apt install steam || error_handler
+#curl -s http://repo.steampowered.com/steam/archive/stable/steam.gpg | sudo tee /usr/share/keyrings/steam.gpg > /dev/null || error_handler
+#echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/steam.gpg] http://repo.steampowered.com/steam/ stable steam' | sudo tee /etc/apt/sources.list.d/steam.list || error_handler
+#apt update && apt install steam || error_handler
+
+wget https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb -P /home/$USR/Downloads || error_handler
+sudo dpkg -i /home/$USR/Downloads/steam.deb || error_handler
+sudo apt -f install || error_handler
