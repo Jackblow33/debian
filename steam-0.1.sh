102418b98abc5a77897c3f5396ddaca25b738a3d
Jackblow33 <jackblow33@hotmail.com>
2025-04-22 20:37:46 -0400

    Update steam-0.1.sh

--- steam-0.1.sh
+++ steam-0.1.sh
@@ -7,6 +7,12 @@
 #echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/steam.gpg] http://repo.steampowered.com/steam/ stable steam' | sudo tee /etc/apt/sources.list.d/steam.list || error_handler
 #apt update && apt install steam || error_handler
 
-wget https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb -P /home/$USR/Downloads || error_handler
-sudo dpkg -i /home/$USR/Downloads/steam.deb || error_handler
-sudo apt -f install || error_handler
+#wget https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb -P /home/$USR/Downloads || error_handler
+#sudo dpkg -i /home/$USR/Downloads/steam.deb || error_handler
+#sudo apt -f install || error_handler
+
+
+sudo apt update
+sudo apt install software-properties-common apt-transport-https
+sudo add-apt-repository non-free
+sudo apt install steam
