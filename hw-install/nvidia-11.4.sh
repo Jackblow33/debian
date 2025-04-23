dcfd62f8f23fd5187ada265b14746a8b97ec5af5
Jackblow33 <jackblow33@hotmail.com>
2025-04-22 21:48:02 -0400

    Update nvidia-11.4.sh

--- hw-install/nvidia-11.4.sh
+++ hw-install/nvidia-11.4.sh
@@ -50,7 +50,7 @@ timer_stop() {
 # Update package list and install necessary packages
 install_dependencies() {
     apt update && apt install -y linux-headers-$(uname -r) gcc make acpid dkms libvulkan1 libglvnd-core-dev pkg-config xorg-dev libglvnd0 libglvnd-dev libc-dev || handle_error  #libglvnd0 libglvnd-dev libc-dev
-    apt install -y mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386  # 32 bit Steam dependencies
+    # apt install -y mesa-vulkan-drivers
 }
 
 # Download NVIDIA driver
