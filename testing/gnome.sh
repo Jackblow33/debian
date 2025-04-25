74164411aa19fa5ff63867a95273c32ed3e2bfcf
Jackblow33 <jackblow33@hotmail.com>
2025-04-24 21:21:39 -0400

    Update gnome.sh

--- testing/gnome.sh
+++ testing/gnome.sh
@@ -51,19 +51,14 @@ for PKG in "${INSTALL_PKGS[@]}"; do
     apt install "$PKG" -y || { echo "Failed to install $PKG"; handle_error; }
 done
 
-# INSTALL FIREFOX NON-ESR - For debian Trixie
-#echo "Installing Firefox browser..."
-#apt install firefox || handle_error
-#source /home/$USR/debian/firefox.sh || handle_error
-
 # INSTALL BRAVE REPO AND INSTALL BRAVE WEB BROWSER
 echo "Installing Brave browser..."
-source /home/$USR/debian/brave.sh || handle_error
+source /home/$USR/debian/testing/brave.sh || handle_error
 sed -i 's|/usr/bin/brave-browser-stable|/usr/bin/brave-browser-stable --password-store=gnome|g' /usr/share/applications/brave-browser.desktop
 
 # INSTALL QEMU-KVM - Virtualisation
 #echo "Installing QEMU-KVM..."
-#source "/home/$USR/debian/qemu-kvm-0.5.sh" || handle_error
+#source "/home/$USR/debian/testing/qemu-kvm-0.5.sh" || handle_error
 
 # UNINSTALL PACKAGES
 for PKG in "${UNINSTALL_PKGS[@]}"; do
