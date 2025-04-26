fc4350d97bc632f67aa231098b65075326c1c3bf
Jackblow33 <jackblow33@hotmail.com>
2025-04-26 08:29:01 -0400

    Update install-2.6.sh

--- install-2.6.sh
+++ install-2.6.sh
@@ -33,7 +33,7 @@ handle_error() {
 
 # Function to display the reboot countdown
 countdown() {
-    echo "Will reboot! Press ctrl+c to cancel."
+    echo "Will reboot (Press ctrl+c to abort)"
     for i in {30..1}
     do
         echo "$i seconds remaining..."
