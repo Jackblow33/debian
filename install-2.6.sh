bc93a5b507ab88a5e43da355a4cbef0e4e4504f4
Jackblow33 <jackblow33@hotmail.com>
2025-04-26 12:00:39 -0400

    Update install-2.6.sh

--- install-2.6.sh
+++ install-2.6.sh
@@ -86,7 +86,7 @@ while true; do
                 ;;
             3)
                 echo "Installing WiFi BCM4360..."
-                source "/home/$USR/debian/hw-install/wifi-bcm43xx.sh"
+                source "/home/$USR/debian/hw-install/wifi-bcm43xx-0.1.sh"
                 ;;
             4)
                 echo "Installing custom kernel $KERNEL from USB..."
