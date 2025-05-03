5825e754307c1fc7019ac3a2363b430d40166d10
Jackblow33 <jackblow33@hotmail.com>
2025-05-03 15:31:58 -0400

    Update nvidia-11.6.sh

--- hw-install/nvidia-11.6.sh
+++ hw-install/nvidia-11.6.sh
@@ -58,13 +58,11 @@ download_nvidia_driver() {
     local driver_file="NVIDIA-Linux-x86_64-${NV_VER}.run"
     download_path="/home/$USR/debian/hw-install/NVIDIA-DRIVERS-ARCHIVES"
     mkdir -p $download_path
-    cd $download_path
-
     # Check if the driver file already exists
     if [ -f "$driver_file" ]; then
         echo "The driver file '$driver_file' already exists in the download path."
     else
-        wget "https://us.download.nvidia.com/XFree86/Linux-x86_64/${NV_VER}/${driver_file}" || handle_error
+        wget -P $download_path "https://us.download.nvidia.com/XFree86/Linux-x86_64/${NV_VER}/${driver_file}" || handle_error
         chmod +x "${driver_file}"
     fi
 }
