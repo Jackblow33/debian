dfa565f303356fa3ea05a7543c3a4f965ef41753
Jackblow33 <jackblow33@hotmail.com>
2025-05-03 19:34:49 -0400

    Update brave.sh

--- brave.sh
+++ brave.sh
@@ -5,7 +5,7 @@
 apt install curl
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-apt update
+apt update -y
 apt install -y brave-browser
 
 # OLD - Keyring fix - now simply uninstalling gnome-keyring package
