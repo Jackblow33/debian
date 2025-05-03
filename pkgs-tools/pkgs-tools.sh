b42315bf65252e1787111a5d8590b2ce617e2984
Jackblow33 <jackblow33@hotmail.com>
2025-05-03 14:01:52 -0400

    Update pkgs-tools.sh

--- pkgs-tools/pkgs-tools.sh
+++ pkgs-tools/pkgs-tools.sh
@@ -1,47 +1,48 @@
 #!/bin/bash
 
-# Add functions & variables
+# pkgs-tools.sh
+# 2025-05-03
 
 USR=$(logname)
 sudo mkdir /home/$USR/debian/pkgs-tools
 
-# Main menu
 while true; do
     clear
     echo -e "\n\n\n\n\n\n\n\n\n\n"  # Clear the screen with new lines
-    echo "                             Please select the options separated by a hyphen (-):"
+    echo "                             Please select an option:"
     echo "                               1. Build packages list from base installation"
-    echo "                               2. Build packages list from full installation"
-    echo "                               3. Build final packages diffs full/base"
-    echo "                               4. Exit"
+    echo "                               2. Debloat full installation before building the list"
+    echo "                               3. Build packages list from full debloated installation"
+    echo "                               4. Build final packages diffs full/base"
+    echo "                               5. Exit"
 
-    read -p "                             Enter your choice (e.g., 1-2-4): " choices
+    read -p "                             Enter your choice: " choice
 
-    IFS='-' read -ra selected_choices <<< "$choices"
-
-    for choice in "${selected_choices[@]}"; do
-        case $choice in
-            1)
-                echo "Building packages list from base installation"
-                source /home/$USR/debian/pkgs-tools/base-pkgs-list.sh
-                ;;
-            2)
-                echo "Building packages list from full installation"
-                source /home/$USR/debian/pkgs-tools/full-pkgs-list.sh
-                ;;
-            3)
-                echo "Building final packages diffs full/base"
-                source /home/$USR/debian/pkgs-tools/diff-pkgs-list.sh
-                ;;
-
-            4)
-                echo "Exiting..."
-                exit 0
-                ;;
-            *)
-                echo "Invalid choice. Please try again."
-                sleep 2
-                ;;
-        esac
-    done
+    case $choice in
+        1)
+            echo "Building packages list from base installation"
+            source /home/$USR/debian/pkgs-tools/base-pkgs-list.sh
+            ;;
+        2)
+            echo "Debloating full installation"
+            source /home/$USR/debian/pkgs-tools/debloat.sh
+            ;;
+        3)
+            echo "Building packages list from full debloated installation"
+            source /home/$USR/debian/pkgs-tools/full-pkgs-list.sh
+            ;;
+        4)
+            echo "Building final packages diffs full/base"
+            source /home/$USR/debian/pkgs-tools/diff-pkgs-list.sh
+            ;;    
+        5)
+            echo "Exiting..."
+            exit 0
+            ;;
+        *)
+            echo "Invalid choice. Please try again."
+            sleep 2
+            ;;
+    esac
 done
+
