018453a1b32ae2f31cd1adbd2bd58e4206fcf11f
Jackblow33 <jackblow33@hotmail.com>
2025-05-04 17:07:34 -0400

    Update OLD_debian-gnome.sh

--- pkgs-tools/OLD_debian-gnome.sh
+++ pkgs-tools/OLD_debian-gnome.sh
@@ -5,27 +5,27 @@
 # Set the input file path
 USR=$(logname)
 input_file="/home/$USR/debian/pkgs-tools/pkgs.list"
-start_time=$SECONDS
+#start_time=$SECONDS
 
-if ! whiptail --title "Debian 13 sid Installation" --yesno "You are about to install Debian 13 sid packages. Would you like to proceed?" 8 60; then
-    echo "Installation cancelled."
-    exit 1
-fi
+#if ! whiptail --title "Debian 13 sid Installation" --yesno "You are about to install Debian 13 sid packages. Would you like to proceed?" 8 60; then
+#    echo "Installation cancelled."
+#    exit 1
+#fi
 
 # Install the packages
 sudo apt-get install -y $(cat "$input_file")
 
-end_time=$SECONDS
-elapsed_time=$((end_time - start_time))
-elapsed_minutes=$((elapsed_time / 60))
-elapsed_seconds=$((elapsed_time % 60))
+#end_time=$SECONDS
+#elapsed_time=$((end_time - start_time))
+#elapsed_minutes=$((elapsed_time / 60))
+#elapsed_seconds=$((elapsed_time % 60))
 
-total_time_message="Total time elapsed: $elapsed_minutes minutes and $elapsed_seconds seconds."
-reboot_message="The packages have been installed successfully. A reboot is required to complete the installation. Press Enter to reboot the system."
+#total_time_message="Total time elapsed: $elapsed_minutes minutes and $elapsed_seconds seconds."
+#reboot_message="The packages have been installed successfully. A reboot is required to complete the installation. Press Enter to reboot the system."
 
-whiptail --title "Installation Complete" --msgbox "$total_time_message\n\n$reboot_message" 12 60
+#whiptail --title "Installation Complete" --msgbox "$total_time_message\n\n$reboot_message" 12 60
 
-read -p "" 
+#read -p "" 
 
-sudo reboot
+#sudo reboot
 
