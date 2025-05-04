e9030cf3cdf3dd925ba2f7f65659147dc75edb17
Jackblow33 <jackblow33@hotmail.com>
2025-05-04 12:47:04 -0400

    Update pkgs-tools.sh

--- pkgs-tools/pkgs-tools.sh
+++ pkgs-tools/pkgs-tools.sh
@@ -6,8 +6,8 @@
 USR=$(logname)
 dir_tools="/home/$USR/debian/pkgs-tools"
 sudo mkdir $dir_tools
+
 while true; do
-    clear
     echo -e "\n\n\n\n\n\n\n\n\n\n"  # Clear the screen with new lines
     echo "                             Please select an option:"
     echo "                               1. Build packages list from base installation"
@@ -20,23 +20,29 @@ while true; do
 
     case $choice in
         1)
+            clear
             echo "Building packages list from base installation"
-            source $dir_tools/base-pkgs-list.sh
+            $dir_tools/base-pkgs-list.sh
             ;;
         2)
+            clear
             echo "Debloating full installation"
-            source $dir_tools/debloat.sh
+            $dir_tools/debloat.sh
             ;;
         3)
+            clear
             echo "Building packages list from full debloated installation"
-            source $dir_tools/full-pkgs-list.sh
+            $dir_tools/full-pkgs-list.sh
             ;;
         4)
             echo "Building final packages diffs full/base"
-            source $dir_tools/diff-pkgs-list.sh
-            ;;    
+            clear
+            $dir_tools/diff-pkgs-list.sh
+            ;;
         5)
+            clear
             echo "Exiting..."
+            sleep 3
             exit 0
             ;;
         *)
@@ -45,4 +51,3 @@ while true; do
             ;;
     esac
 done
-
