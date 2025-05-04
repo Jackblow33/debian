823c0240ee4957573325f285a5496f4ed2eaf6e4
Jackblow33 <jackblow33@hotmail.com>
2025-05-04 13:15:33 -0400

    Update base-pkgs-list.sh

--- pkgs-tools/base-pkgs-list.sh
+++ pkgs-tools/base-pkgs-list.sh
@@ -5,9 +5,17 @@
 
 # Set the output directory & file path
 USR=$(logname)
+timestamp=$(date +"%Y-%m-%d-%H-%M")
 output_dir="/home/$USR/Downloads/pkgs-tools"
 output_file="$output_dir/base_pkgs.list"
-mkdir $output_dir
+mkdir -p $output_dir
+
+# Check if the output file already exists
+if [ -f "$output_file" ]; then
+    renamed_file="$output_dir/base_pkgs_$timestamp.list"
+    mv "$output_file" "$renamed_file"
+    echo "Existing file renamed to $renamed_file"
+fi
 
 # Get the list of installed packages
 installed_packages=$(apt list --installed | cut -d'/' -f1 | tail -n +2)
@@ -16,4 +24,3 @@ installed_packages=$(apt list --installed | cut -d'/' -f1 | tail -n +2)
 echo "$installed_packages" >> "$output_file"
 
 echo "Cleaned list saved to $output_file"
-
