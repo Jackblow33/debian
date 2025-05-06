e190dc133ae613ce65a819ca00ab0e3a413c4a67
Jackblow33 <jackblow33@hotmail.com>
2025-05-05 21:55:16 -0400

    Update alpha-debian-gnome.sh

--- pkgs-tools/alpha-debian-gnome.sh
+++ pkgs-tools/alpha-debian-gnome.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Debian sid base gnome desktop environment packages installer (Linux from scratch).
+# Debian sid base gnome desktop environment packages installer over a base minimal installation.
 
 # Set the input file path
 USR=$(logname)
+LOG_DIR="/home/$USR/debian/LOGS"
+LOG_FILE="install.log"
+mkdir $LOG_FILE
 input_file="/home/$USR/debian/pkgs-tools/tasksel_pkgs.list"
+
 start_time=$SECONDS
 
 if ! whiptail --title "Debian 13 sid Installation" --yesno "You are about to install Debian 13 sid packages. Would you like to proceed?" 8 60; then
@@ -20,11 +24,9 @@ elapsed_time=$((end_time - start_time))
 elapsed_minutes=$((elapsed_time / 60))
 elapsed_seconds=$((elapsed_time % 60))
 
-total_time_message="Total time elapsed: $elapsed_minutes minutes and $elapsed_seconds seconds."
-reboot_message="The packages have been installed successfully. A reboot is required to complete the installation. Press Enter to reboot the system."
+total_time_message="Gnone install, total time elapsed: $elapsed_minutes minutes and $elapsed_seconds seconds."
+echo "$total_time_message" >> "$LOG_DIR/$LOG_FILE"
 
-whiptail --title "Installation Complete" --msgbox "$total_time_message\n\n$reboot_message" 12 60
+echo "$total_time_message"
 
-read -p "" 
 
-sudo reboot
