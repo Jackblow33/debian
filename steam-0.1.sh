26e0245e24f3ff83cf253311119b57b7bb9eadc7
Jackblow33 <jackblow33@hotmail.com>
2025-05-08 20:48:04 -0400

    Update steam-0.1.sh

--- steam-0.1.sh
+++ steam-0.1.sh
@@ -3,12 +3,12 @@
 # Debian Steam install
 
 
-apt --fix-broken install -y
-wget https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb -P /home/$USR/Downloads || error_handler
-sudo dpkg -i /home/$USR/Downloads/steam.deb || error_handler
+#apt --fix-broken install -y
+#wget https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb -P /home/$USR/Downloads || error_handler
+#sudo dpkg -i /home/$USR/Downloads/steam.deb || error_handler
 #sudo apt -f install || error_handler
 
-#dpkg --add-architecture i386
+
 #dpkg --add-architecture i386
 #apt install mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
 #apt install steam-installer
