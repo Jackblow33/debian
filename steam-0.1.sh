a6ea5cd38b5df3afbae879ca2611dc2924c49673
Jackblow33 <jackblow33@hotmail.com>
2025-05-09 19:05:55 -0400

    Update steam-0.1.sh

--- steam-0.1.sh
+++ steam-0.1.sh
@@ -11,6 +11,7 @@
 
 sudo apt install nvidia-driver-libs:i386
 dpkg --add-architecture i386
+sudo apt update
 apt install mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
 apt install steam-installer
 #  https://wiki.debian.org/Steam
