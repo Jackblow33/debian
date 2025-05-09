1885039b447f249ac922558043beeac0cb5dd8b6
Jackblow33 <jackblow33@hotmail.com>
2025-05-08 20:35:23 -0400

    Update steam-0.1.sh

--- steam-0.1.sh
+++ steam-0.1.sh
@@ -3,16 +3,13 @@
 # Debian Steam install
 
 
-#curl -s http://repo.steampowered.com/steam/archive/stable/steam.gpg | sudo tee /usr/share/keyrings/steam.gpg > /dev/null || error_handler
-#echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/steam.gpg] http://repo.steampowered.com/steam/ stable steam' | sudo tee /etc/apt/sources.list.d/steam.list || error_handler
-#apt update && apt install steam || error_handler
-
+apt --fix-broken instal -y
 wget https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb -P /home/$USR/Downloads || error_handler
-#sudo dpkg -i /home/$USR/Downloads/steam.deb || error_handler
+sudo dpkg -i /home/$USR/Downloads/steam.deb || error_handler
 #sudo apt -f install || error_handler
 
-dpkg --add-architecture i386
-dpkg --add-architecture i386
-apt install mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
-apt install steam-installer
+#dpkg --add-architecture i386
+#dpkg --add-architecture i386
+#apt install mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
+#apt install steam-installer
 #  https://wiki.debian.org/Steam
