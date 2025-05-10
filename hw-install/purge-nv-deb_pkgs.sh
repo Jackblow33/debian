00c6ec0721d2504fd88ce54c54ce09a6b99a7878
Jackblow33 <jackblow33@hotmail.com>
2025-05-10 10:18:28 -0400

    Create purge-nv-deb_pkgs.sh

---
+++ hw-install/purge-nv-deb_pkgs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#https://www.reddit.com/r/debian/comments/16q9ok0/trying_to_purge_nvidia/?rdt=62828
+
+
+# Remove (add purge) 99% of the nvidia packages
+
+  sudo apt-get remove --purge nvidia-installer-cleanup \
+  nvidia-persistenced
+
+# Remove (and purge) the auto-installed nvidia stuff
+
+  sudo apt-get autoremove --purge
