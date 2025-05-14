3b28f54c07ca8fd554f22f0abc08afac38cdb9f7
Jackblow33 <jackblow33@hotmail.com>
2025-05-14 18:24:35 -0400

    Update brave.sh

--- brave.sh
+++ brave.sh
@@ -2,7 +2,7 @@
 
 # Debian, Ubuntu, Mint - 2025-05-07
 
-apt purge -y gnome-keyring && apt autoremove -y
+
 apt install -y curl
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
@@ -10,4 +10,5 @@ apt update -y
 apt install -y brave-browser
 
 # OLD - Keyring fix - now simply uninstalling gnome-keyring package
-sed -i 's|/usr/bin/brave-browser-stable|/usr/bin/brave-browser-stable --password-store=gnome|g' /usr/share/applications/brave-browser.desktop
+# sed -i 's|/usr/bin/brave-browser-stable|/usr/bin/brave-browser-stable --password-store=gnome|g' /usr/share/applications/brave-browser.desktop
+apt purge -y gnome-keyring && apt autoremove -y
