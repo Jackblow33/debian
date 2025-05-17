c1a7a51b8e2b009ad3ce88b685c0b759a6a6ecf3
Jackblow33 <jackblow33@hotmail.com>
2025-05-17 18:00:20 -0400

    Update post-install-menu.sh

--- post-install/post-install-menu.sh
+++ post-install/post-install-menu.sh
@@ -6,11 +6,15 @@
 USR=$(logname)
 startup_file="/home/$USR/.config/autostart/software_installer.desktop"
 autostart_dir=$(dirname "$startup_file")
+menu_script="/home/$USR/debian/post-install/menu-0.4.1.py"
 
 # Create the autostart directory if it doesn't exist
 if [ ! -d "$autostart_dir" ]; then
     mkdir -p "$autostart_dir"
 fi
+
+# Make the menu-0.4.1.sh script executable
+chmod +x "$menu_script"
 
 # Create the startup file
 cat << EOF > "$startup_file"
