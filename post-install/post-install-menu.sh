970056c617603cc050e02c10d22f1d91ea7c4303
Jackblow33 <jackblow33@hotmail.com>
2025-05-17 16:31:36 -0400

    Create post-install-menu.sh

---
+++ post-install/post-install-menu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# post-install-menu.sh
+# 2025-05-17
+
+USR=$(logname)
+startup_file="/home/$USR/.config/autostart/software_installer.desktop"
+autostart_dir=$(dirname "$startup_file")
+
+# Create the autostart directory if it doesn't exist
+if [ ! -d "$autostart_dir" ]; then
+    mkdir -p "$autostart_dir"
+fi
+
+# Create the startup file
+cat << EOF > "$startup_file"
+[Desktop Entry]
+Name=Software Installer
+Exec=python3 /home/$USR/debian/post-install/menu-0.4.1.py
+Terminal=false
+Type=Application
+Hidden=false
+EOF
+
+echo "Software Installer gui will now load at boot."
