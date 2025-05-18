c6b03e8a21a5feda8ed7db75543b7f478ab85b18
Jackblow33 <jackblow33@hotmail.com>
2025-05-18 19:48:28 -0400

    Update brave.sh

--- brave.sh
+++ brave.sh
@@ -2,7 +2,8 @@
 
 # Brave web browser install. Debian, Ubuntu, Mint - 2025-05-14
 
-gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg
+#sudo chown -R $USR:$USR /home/$USR/.local || check
+#gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg
 apt install -y curl
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
