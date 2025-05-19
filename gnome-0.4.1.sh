bd5aec3067b4cfb6525d89552319c18c35543eb2
Jackblow33 <jackblow33@hotmail.com>
2025-05-18 20:49:41 -0400

    Update gnome-0.4.1.sh

--- gnome-0.4.1.sh
+++ gnome-0.4.1.sh
@@ -23,8 +23,9 @@ input_file="/home/$USR/debian/pkgs-tools/tasksel_pkgs.list"
 sudo apt-get install -y $(cat "$input_file") || handle_error
 
 sudo mkdir -p /home/$USR/.local/state/wireplumber || check
-#sudo chown -R $USR:$USR /home/$USR/.local || check
 sudo chown -R $USR:$USR /home/$USR/.local/state/wireplumber || check
+sudo chown -R $USR:$USR /home/$USR/.local || check
+gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg
 }
 
 
