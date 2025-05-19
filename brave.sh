41bdf726182da866562faa5017d6d5dd3c68ca76
Jackblow33 <jackblow33@hotmail.com>
2025-05-19 10:36:07 -0400

    Update brave.sh

--- brave.sh
+++ brave.sh
@@ -23,6 +23,8 @@ sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://b
 sudo echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update -y
 sudo apt install -y brave-browser
+press_enter
+sudo reboot
 
 # Keyring quirk fix
 # apt purge -y gnome-keyring && apt autoremove -y
