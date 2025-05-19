7e87698d1b30b43d2585aa50bd8f892cd64d532e
Jackblow33 <jackblow33@hotmail.com>
2025-05-18 22:41:54 -0400

    Update brave.sh

--- brave.sh
+++ brave.sh
@@ -8,6 +8,7 @@
 sudo systemctl daemon-reload
 sudo systemctl start gnome-keyring-daemon
 sudo systemctl enable gnome-keyring-daemon.service
+# Reboot ???
 sudo apt install -y curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 sudo echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
