b06bb0f2b837f754f20c9ab7995f2346c87d7d4f
Jackblow33 <jackblow33@hotmail.com>
2025-05-19 14:18:46 -0400

    Update gnome-keyring-setup.sh

--- gnome-keyring-setup.sh
+++ gnome-keyring-setup.sh
@@ -30,6 +30,6 @@ sudo chown -R $USR:$USR /home/$USR/.local || handle_error
 sudo systemctl daemon-reload || handle_error
 sudo systemctl start gnome-keyring-daemon || handle_error
 sudo systemctl enable gnome-keyring-daemon.service || handle_error
-sudo systemctl status gnome-keyring-daemon.service || handle_error
+sudo systemctl status gnome-keyring-daemon.service
 press_enter
 #sudo nano /etc/systemd/system/gnome-keyring-daemon.service
