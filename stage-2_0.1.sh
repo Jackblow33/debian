cb2c326411de98b78c115ec405a468e732716bd7
Jackblow33 <jackblow33@hotmail.com>
2025-05-22 11:02:33 -0400

    Update stage-2_0.1.sh

--- stage-2_0.1.sh
+++ stage-2_0.1.sh
@@ -4,8 +4,9 @@
 # 2025-05-22
 
 stage_2_installer() {
-    stage_2="/home/jack/.config/autostart/stage-2-installer.desktop"
-    cat << EOF > "$stage_2" || handle_error
+    stage_2="/home/$USR/.config/autostart/stage-2-installer.desktop"
+    sudo mkdir "/home/$USR/.config/autostart"
+    cat << EOF > "$stage_2"
 [Desktop Entry]
 Name=Stage 2 customm installer
 Exec=/usr/bin/kgx -- /home/jack/debian/extras.sh
