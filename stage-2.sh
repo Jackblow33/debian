9e84794d2f98ae7a35c6776276582a7ad822536e
Jackblow33 <jackblow33@hotmail.com>
2025-05-21 21:15:47 -0400

    Add files via upload

---
+++ stage-2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# stage-2.sh
+# 2025-05-21
+
+# Handle errors
+handle_error() {
+    echo "Error occurred in the script. Exiting."
+    exit 1
+}
+
+stage_2_installer() {
+    stage_2="/etc/systemd/system/stage-2-installer.service"
+    cat << EOF > "$stage_2" || handle_error
+[Unit]
+Description=Stage 2 custom installer script
+After=graphical.target
+Wants=graphical.target
+
+[Service]
+ExecStart=/usr/bin/gnome-terminal -- /home/jack/debian/extras.sh
+Type=oneshot
+RemainAfterExit=yes
+
+[Install]
+WantedBy=graphical.target
+EOF
+
+    sudo systemctl daemon-reload || handle_error
+    sudo systemctl enable stage-2-installer.service || handle_error
+    sudo systemctl start stage-2-installer.service || handle_error
+}
+
+stage_2_installer
